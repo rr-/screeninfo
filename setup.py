ba40c1f21e710da259ee6c40671690679c7c7a99
rr- <mkurczew@gmail.com>
2015-07-02 13:50:53 +0200

    Upgraded to 0.2.1

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'screeninfo',
   packages = ['screeninfo'],
-  version = '0.2',
+  version = '0.2.1',
   description = 'Fetch location and size of physical screens.',
   author = 'rr-',
   author_email = 'rr-@sakuya.pl',
   url = 'https://github.com/rr-/screeninfo',
-  download_url = 'https://github.com/rr-/screeninfo/tarball/0.2',
+  download_url = 'https://github.com/rr-/screeninfo/tarball/0.2.1',
   keywords = ['screen', 'monitor', 'desktop'],
   classifiers = [],
 )
