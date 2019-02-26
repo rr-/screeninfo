a4b8a3e75fab8fe777f2f5bea76832421af7055d
rr- <rr-@sakuya.pl>
2019-02-26 21:28:58 +0100

    style: remove _ENUMERATOR_TO_MODULE_PATH

--- screeninfo/screeninfo.py
+++ screeninfo/screeninfo.py
@@ -1,36 +1,27 @@
 import importlib
 import typing as T
+from pathlib import Path
 
 from screeninfo.common import Enumerator, Monitor, ScreenInfoError
 
-_ENUMERATOR_TO_MODULE_PATH = {
-    Enumerator.Windows: "screeninfo.enumerators.windows",
-    Enumerator.Cygwin: "screeninfo.enumerators.cygwin",
-    Enumerator.Xrandr: "screeninfo.enumerators.xrandr",
-    Enumerator.Xinerama: "screeninfo.enumerators.xinerama",
-    Enumerator.DRM: "screeninfo.enumerators.drm",
-    Enumerator.OSX: "screeninfo.enumerators.osx",
-}
 
-
-def _get_monitors(enumerator_module_path: str) -> T.List[Monitor]:
+def _get_monitors(enumerator: Enumerator) -> T.List[Monitor]:
+    enumerator_module_path = f"screeninfo.enumerators.{enumerator.value}"
     module = importlib.import_module(enumerator_module_path)
     return list(module.enumerate_monitors())
 
 
 def get_monitors(name: T.Optional[Enumerator] = None) -> T.List[Monitor]:
+    """Returns a list of :class:`Monitor` objects based on active monitors."""
     enumerator = Enumerator(name) if name is not None else None
 
-    """Returns a list of :class:`Monitor` objects based on active monitors."""
     if enumerator is not None:
-        if enumerator not in _ENUMERATOR_TO_MODULE_PATH:
-            raise ScreenInfoError("Unknown enumerator: " + enumerator.value)
+        return _get_monitors(enumerator)
 
-        return _get_monitors(_ENUMERATOR_TO_MODULE_PATH[enumerator])
-
-    for module_path in _ENUMERATOR_TO_MODULE_PATH.values():
+    for enumerator in Enumerator:
         try:
-            return _get_monitors(module_path)
+            return _get_monitors(enumerator)
         except Exception:
             pass
+
     raise ScreenInfoError("No enumerators available")
