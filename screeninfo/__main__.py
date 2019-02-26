73a58b6cf97d1fa6aa2db4fea66fa2e26e0705cc
rr- <rr-@sakuya.pl>
2019-02-26 19:39:22 +0100

    add ability to specify enumerator in cli args

--- screeninfo/__main__.py
+++ screeninfo/__main__.py
@@ -1,8 +1,21 @@
+import argparse
+
+from screeninfo.common import Enumerator
+
 from .screeninfo import get_monitors
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "enumerator", nargs="?", choices=[item.value for item in Enumerator]
+    )
+    return parser.parse_args()
+
+
 def main():
-    for m in get_monitors():
+    args = parse_args()
+    for m in get_monitors(args.enumerator):
         print(str(m))
 
 
