7ddc44168b71a01fffdf666bb242491591c55f1b
Rodney Lorrimar <dev@rodney.id.au>
2019-07-28 18:54:59 +1000

    setup.py: Add screeninfo.enumerators to packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="screeninfo",
-    packages=["screeninfo"],
+    packages=["screeninfo", "screeninfo.enumerators"],
     version="0.3.1",
     description="Fetch location and size of physical screens.",
     author="rr-",
