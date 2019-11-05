44ac8456d3e7855c0e509511e717dd42f15c8650
rr- <rr-@sakuya.pl>
2019-11-05 12:24:52 +0100

    release 0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="screeninfo",
     packages=["screeninfo", "screeninfo.enumerators"],
-    version="0.4.1",
+    version="0.5",
     description="Fetch location and size of physical screens.",
     author="rr-",
     author_email="rr-@sakuya.pl",
