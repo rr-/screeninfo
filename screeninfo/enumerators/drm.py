rr-/screeninfo

commit edff3ca8fa298139dd52ee8f04f0dd8846dd7dc0

drm: make sure to close fd

screeninfo/enumerators/drm.py
@@ -225,18 +225,24 @@ def enumerate_monitors() -> T.Iterable[Monitor]:
         if fd < 0:
             continue
 
-        res = libdrm.drmModeGetResources(fd)
-        if not res:
-            raise ScreenInfoError("Failed to get drm resources")
+        try:
+            res = libdrm.drmModeGetResources(fd)
+            if not res:
+                raise ScreenInfoError("Failed to get drm resources")
 
-        res = res.contents
-        res.fd = fd
-        res.needs_free = True
-        for connector in res.connectors:
-            if connector.connection == DrmModeConnector.DRM_MODE_CONNECTED:
-                crtc = connector.encoder.crtc
-                yield Monitor(
-                    x=crtc.x, y=crtc.y, width=crtc.width, height=crtc.height
-                )
+            res = res.contents
+            res.fd = fd
+            res.needs_free = True
 
-        os.close(fd)
+            for connector in res.connectors:
+                if connector.connection != DrmModeConnector.DRM_MODE_CONNECTED:
+                    crtc = connector.encoder.crtc
+                    yield Monitor(
+                        x=crtc.x,
+                        y=crtc.y,
+                        width=crtc.width,
+                        height=crtc.height,
+                    )
+
+        finally:
+            os.close(fd)

screeninfo/enumerators/x11.py
@@ -48,6 +48,7 @@ def enumerate_monitors() -> T.Iterable[Monitor]:
             yield Monitor(
                 x=info.x, y=info.y, width=info.width, height=info.height
             )
+
     finally:
         xlib.XCloseDisplay.restype = ctypes.POINTER(ctypes.c_void_p)
         xlib.XCloseDisplay(display)

commit a4be804d7f198b8b3e9158399dc1db10c5cd8762

drm: fix checking for connection
Broken in edff3ca8fa298139dd52ee8f04f0dd8846dd7dc0

screeninfo/enumerators/drm.py
@@ -224,7 +224,7 @@ def enumerate_monitors() -> T.Iterable[Monitor]:
             res.needs_free = True
 
             for connector in res.connectors:
-                if connector.connection != DrmModeConnector.DRM_MODE_CONNECTED:
+                if connector.connection == DrmModeConnector.DRM_MODE_CONNECTED:
                     crtc = connector.encoder.crtc
                     yield Monitor(
                         x=crtc.x,