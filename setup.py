3f502a6672694e905a0202ff0db1767ea8ca4ed2
rr- <rr-@sakuya.pl>
2020-10-23 09:55:39 +0200

    release 0.6.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="screeninfo",
     packages=["screeninfo", "screeninfo.enumerators"],
-    version="0.6.5",
+    version="0.6.6",
     description="Fetch location and size of physical screens.",
     author="rr-",
     author_email="rr-@sakuya.pl",
