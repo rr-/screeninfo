ca4fbdde91383d9054dbaaf1fa3025ae55515f57
Joe Stanley <33275230+engineerjoe440@users.noreply.github.com>
2020-11-17 01:58:01 -0800

    setup: add long description and classifiers

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 from setuptools import setup
 
+# Collect the Current README docs for PyPI
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="screeninfo",
     packages=["screeninfo", "screeninfo.enumerators"],
     version="0.6.6",
     description="Fetch location and size of physical screens.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="rr-",
     author_email="rr-@sakuya.pl",
     url="https://github.com/rr-/screeninfo",
     keywords=["screen", "monitor", "desktop"],
-    classifiers=[],
+    classifiers=[
+        'Operating System :: MacOS :: MacOS X :: PyOBJus',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Microsoft :: Windows :: Cygwin',
+        'Operating System :: Linux :: X11',
+        'Operating System :: Linux :: DRM :: Experimental'
+    ],
     install_requires=[
         "dataclasses ; python_version<'3.7'",
         'Cython ; sys_platform=="darwin"',
