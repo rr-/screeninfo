1c83d35191cb46ef4c3b9ff1d9d4c9be503a2816
rr- <rr-@sakuya.pl>
2020-12-31 10:07:34 +0100

    setup.py: remove Cython dependency

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(
     ],
     install_requires=[
         "dataclasses ; python_version<'3.7'",
-        'Cython ; sys_platform=="darwin"',
         'pyobjc-framework-Cocoa ; sys_platform=="darwin"',
     ],
 )
