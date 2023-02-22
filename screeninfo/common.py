26647c49000397236c0fbd023e90a6de80233d3a
Florin Pop <florin@ae.studio>
2023-02-22 10:17:56 +0100

    Compute scale factor and size in mm on macOS Estimate ppmm

--- screeninfo/common.py
+++ screeninfo/common.py
@@ -11,17 +11,26 @@ class Monitor:
     y: int
     width: int
     height: int
+    scale: float = 1
     width_mm: T.Optional[int] = None
     height_mm: T.Optional[int] = None
     name: T.Optional[str] = None
     is_primary: T.Optional[bool] = None
+
+    @property
+    def ppmm(self) -> T.Optional[T.Tuple[float, float]]:
+        if self.width_mm is None or self.height_mm is None:
+            return None
+        return (self.width/self.width_mm, self.height/self.height_mm)
 
     def __repr__(self) -> str:
         return (
             f"Monitor("
             f"x={self.x}, y={self.y}, "
             f"width={self.width}, height={self.height}, "
+            f"scale={self.scale}, "
             f"width_mm={self.width_mm}, height_mm={self.height_mm}, "
+            f"ppmm={self.ppmm!r}, "
             f"name={self.name!r}, "
             f"is_primary={self.is_primary}"
             f")"
--- screeninfo/enumerators/osx.py
+++ screeninfo/enumerators/osx.py
@@ -10,7 +10,8 @@ def check_primary(screens: T.Any, screen: T.Any) -> bool:
 
 
 def enumerate_monitors() -> T.Iterable[Monitor]:
-    from AppKit import NSScreen
+    from AppKit import NSScreen, NSDeviceSize
+    from Quartz import CGDisplayScreenSize
 
     screens = NSScreen.screens()
 
@@ -19,10 +20,16 @@ def enumerate_monitors() -> T.Iterable[Monitor]:
         if callable(f):
             f = f()
 
+        description = screen.deviceDescription()
+        width, height = description[NSDeviceSize].sizeValue()
+        width_mm, height_mm = CGDisplayScreenSize(description["NSScreenNumber"])
         yield Monitor(
             x=int(f.origin.x),
             y=int(f.origin.y),
-            width=int(f.size.width),
-            height=int(f.size.height),
+            width=width,
+            height=height,
+            scale=screen.backingScaleFactor(),
+            width_mm=width_mm,
+            height_mm=height_mm,
             is_primary=check_primary(screens, screen),
         )
